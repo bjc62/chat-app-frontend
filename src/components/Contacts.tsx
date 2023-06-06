ab99ffc1eaabd715223030cd89bc503aed565d76
Ben <benjencheng62@gmail.com>
2023-06-06 08:13:39 +0800

    Contacts component to fetch chatHistory to display who has sent message before

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,16 +1,49 @@
+// An React component that displays a list of contacts
+// Upon clicking on a contact, the Chat component will be displayed
+// Other than this component itself, FindNewContact component can also be selecting a contact
+// and cause a re-render to this component
+import React from "react";
+import axios from "axios";
+import ChatHistory from "../types/ChatHistory";
 import User from "../types/User";
+import { UserContext } from "../context/UserContext";
 
 interface ContactsProps {
-  contacts: User[];
+  setSelectedToUserEmail: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const Contacts: React.FC<ContactsProps> = ({ contacts }) => {
+const Contacts: React.FC<ContactsProps> = ({ setSelectedToUserEmail }) => {
+  const [chatHistories, setChatHistories] = React.useState<ChatHistory[]>();
+  const { user } = React.useContext(UserContext);
+
+  React.useEffect(() => {
+    const makeAPICall = async () => {
+      const response = await axios.get("http://localhost:3001/chatHistory", {
+        params: { userEmail: user?.email },
+      });
+      console.log(`fetched chatHistory: ${JSON.stringify(response.data)}`);
+      setChatHistories(response.data);
+    };
+    makeAPICall();
+  }, []);
+
+  const handleClick = (selectedToUserEmail: string) => {
+    setSelectedToUserEmail(selectedToUserEmail);
+  };
+
   return (
     <>
       <header>Chat History</header>
       <ul>
-        {contacts.map((user) => (
-          <li key={user.email}>{user.email}</li>
+        {chatHistories?.map((chatHistory) => (
+          <li key={chatHistory.theOtherReceipient}>
+            <a
+              href="#"
+              onClick={() => handleClick(chatHistory.theOtherReceipient)}
+            >
+              {chatHistory.theOtherReceipient}
+            </a>
+          </li>
         ))}
       </ul>
     </>
