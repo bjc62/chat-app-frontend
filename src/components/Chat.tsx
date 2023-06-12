2abc04a587d736611d44e669e812eb5b0b3762df
Ben <benjencheng62@gmail.com>
2023-06-12 16:56:00 +0800

    lift the message state from Chat to ChatHomePage component, so that MessageControl can also update the messages state

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,73 +1,21 @@
+// React component to display list of messages
+// List of messages is passed to this component as props and controlled by its parent: ChatHomePage
+// Whenever socker receive new messages, socket will update the messages and cause re-render to this component
+// After user select which use to chat with, MessageControal should update the selectedToUserEmail state
+// and when selectedToUserEmail state is updated, it should trigger ChatHomePage to re-fetch the chat history
+// between the current user and the selected user, and then update the messages
 import axios from "axios";
 import React from "react";
 import Message from "../types/Message";
-import { SocketContext } from "../context/SocketContext";
 
 interface ChatProps {
-  fromUserEmail: string;
-  toUserEmail: string;
-  timestamp: number;
+  messages: Message[];
 }
 
-const fetchChatMessage = async (
-  fromUserEmail: string,
-  toUserEmail: string,
-  timestamp: number
-): Promise<Message[]> => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_API_URL}:3001/historicalMessage`,
-    {
-      params: {
-        fromUserEmail,
-        toUserEmail,
-        timestamp,
-      },
-    }
-  );
-  return response.data;
-};
-
 const Chat: React.FC<ChatProps> = (props: ChatProps) => {
-  const [messages, setMessages] = React.useState<Message[]>([]);
-
-  const { socket } = React.useContext(SocketContext);
-
-  React.useEffect(() => {}, []);
-
-  // fetch historical messages
-  React.useEffect(() => {
-    console.log(
-      `Chat component fetching message for ${props.fromUserEmail}:${props.toUserEmail}:${props.timestamp}`
-    );
-    const makeAPIcall = async () => {
-      const response = await fetchChatMessage(
-        props.fromUserEmail,
-        props.toUserEmail,
-        props.timestamp
-      );
-      setMessages(response);
-    };
-    makeAPIcall();
-  }, [props.toUserEmail]);
-
-  // socket to register private_message event
-  React.useEffect(() => {
-    console.log("Chat component subscribing private_message event");
-    const privateMessageHandler = (message: Message) => {
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
-
-    socket?.on("private_message", privateMessageHandler);
-
-    // Cleanup
-    return () => {
-      socket?.off("private_message", privateMessageHandler);
-    };
-  }, [socket]); // 'socket' as dependency to re-register the handler if 'socket' changes
-
   return (
     <ul>
-      {messages.map((message) => {
+      {props.messages.map((message) => {
         return (
           <li
             key={`${message.fromUserEmail}:${message.toUserEmail}:${message.timestamp}`}
--- src/components/ChatHomePage.tsx
+++ src/components/ChatHomePage.tsx
@@ -1,15 +1,80 @@
+// React component that manages many child components
+// It manages few state and pass these states to its children components
+// States:
+//   - selectedToUserEmail: the email of the user that the current user is chatting with
+//                          it can be updated by Contacts and FineNewContat components
+//   - messages: the messages between the current user and the selected user
+//               it can be updated by selectedToUserEmail state and MessageControl component
+//               selectedToUserEmail will load completely different messagew
+//               while MessageControl will append new messages as user input
 import React from "react";
 import Contacts from "./Contacts";
 import Chat from "./Chat";
 import MessageControl from "./MessageControl";
 import { SocketContext, SocketProvider } from "../context/SocketContext";
 import { UserContext } from "../context/UserContext";
-import User from "../types/User";
 import FindNewContact from "./FIndNewContact";
+import axios from "axios";
+import Message from "../types/Message";
 
 const ChatHomePage: React.FC = () => {
+  const { user } = React.useContext(UserContext);
+  const { socket } = React.useContext(SocketContext);
+
   const [selectedToUserEmail, setSelectedToUserEmail] =
     React.useState<string>("");
+  const [messages, setMessages] = React.useState<Message[]>([]);
+
+  // socket to register private_message event
+  React.useEffect(() => {
+    console.log("Chat component subscribing private_message event");
+
+    const privateMessageHandler = (message: Message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    socket?.on("private_message", privateMessageHandler);
+
+    // Cleanup
+    return () => {
+      socket?.off("private_message", privateMessageHandler);
+    };
+  }, [socket]); // 'socket' as dependency to re-register the handler if 'socket' changes
+
+  // fetch messages when selectedToUserEmail is updated
+  React.useEffect(() => {
+    console.log(
+      `selectedToUserEmail is updated: ${selectedToUserEmail}, fetching messages...`
+    );
+    fetchAndSetMessage();
+  }, [selectedToUserEmail]);
+
+  const fetchAndSetMessage = async () => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/historicalMessage`,
+      {
+        params: {
+          fromUserEmail: user?.email,
+          toUserEmail: selectedToUserEmail,
+          timestamp: new Date(
+            new Date().setDate(new Date().getDate() - 7)
+          ).getTime(),
+        },
+      }
+    );
+    if (response.status === 200) {
+      setMessages(response.data);
+    } else {
+      alert(
+        `failed fetching message for fromUserEmail: ${
+          user?.email
+        }, toUserEmail: ${selectedToUserEmail}, timestamp: ${new Date(
+          new Date().setDate(new Date().getDate() - 7)
+        ).getTime()}. Setting messages to an empty array.`
+      );
+      setMessages([]);
+    }
+  };
 
   return (
     <>
@@ -20,14 +85,9 @@ const ChatHomePage: React.FC = () => {
         ></FindNewContact>
       </div>
 
+      {/* todo: not render this div where there is no selectedToUserEmail*/}
       <div id="Chat">
-        <Chat
-          fromUserEmail="lpaben62@gmail.com"
-          toUserEmail={selectedToUserEmail}
-          timestamp={new Date(
-            new Date().setDate(new Date().getDate() - 7)
-          ).getTime()}
-        ></Chat>
+        <Chat messages={messages}></Chat>
         {/* {selectedToUserEmail.length > 0 && (
           <MessageControl
             selectedToUserEmail={selectedToUserEmail}
